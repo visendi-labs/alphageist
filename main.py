07ed690e331547a793fa5cc25366648482b9918b
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2023-04-21 14:36:17 +0200

    Add main structure

---
+++ main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.document_loaders import TextLoader
+from langchain.llms import OpenAI
+
+load_dotenv()
+
+loader = TextLoader('test/data/ww2.txt')
+
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+llm = OpenAI(temperature=0.0, model_name="gpt-3.5-turbo")
+
+query = """What did England do during the second world war? Answer in the following format: 
+[{"action": <short descriprtion>, "date": <the date when the action occured>}, ...]
+
+"""
+print(index.query(query, llm=llm))
