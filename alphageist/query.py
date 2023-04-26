a87293847cd83df2c48db573c032ef38d9d9880e
Rasmus Nordström <nordstrom.rasmus@gmail.com>
2023-04-26 15:24:02 +0200

    Remove unused function query_files

--- alphageist/query.py
+++ alphageist/query.py
@@ -5,22 +5,6 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from alphageist.doc_generator import get_loaders
 
-def query_files(query: str, path: str):
-    """Old function"""
-    llm = ChatOpenAI(temperature=0.0, model_name="gpt-3.5-turbo")
-    loaders = get_loaders(path)
-    if len(loaders) == 0:
-        raise Exception("error: no loaders found")
-
-    print(f"Number of loaders: {len(loaders)}\n")
-    index = VectorstoreIndexCreator().from_loaders(loaders)
-
-    response = index.query_with_sources(query, llm=llm)
-    response["answer"] = response["answer"].rstrip("\n") # Remove trailing newline character
-
-    return response
-
-    
 def query_docs(docs, query) -> dict: 
 
     embedding = OpenAIEmbeddings()
